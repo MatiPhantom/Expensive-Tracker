7713de75bbe06667baab89a9efcb045ac342be11
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-01 21:28:33 -0500

    Creacion de modelo

---
+++ src/main/java/org/example/Bill.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Bill {
+    public static int COUNT= 0;
+
+    private int id;
+    private String description;
+    private LocalDate date = LocalDate.now();
+    private double amount;
+
+    public Bill(String description, double amount) {
+        this.id=COUNT++;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
